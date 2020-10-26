f1f62f342540d869321b9b75b0f9ec9b73323d93
LenaKurasheva <lena.kurasheva@gmail.com>
2020-10-26 12:41:34 +0300

    add removeObserver in SplashViewModel

--- app/src/main/java/com/lenakurasheva/notes/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/lenakurasheva/notes/ui/splash/SplashViewModel.kt
@@ -1,15 +1,32 @@
 package com.lenakurasheva.notes.ui.splash
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import com.lenakurasheva.notes.data.Repository
+import com.lenakurasheva.notes.data.entity.User
 import com.lenakurasheva.notes.data.errors.NoAuthException
 import com.lenakurasheva.notes.ui.base.BaseViewModel
 
 class SplashViewModel : BaseViewModel<Boolean?, SplashViewState>() {
-    fun requestUser(){
-        Repository.getCurrentUser().observeForever{    //TODO remove observer
-            viewStateLiveData.value = it?.let { SplashViewState(true) } ?: let {
+
+    var currentUserLiveData: LiveData<User?>? = null
+    private val currentUserObserver = object : Observer<User?> {
+        override fun onChanged(result: User?) {
+            viewStateLiveData.value = result?.let { SplashViewState(true) } ?: let {
                 SplashViewState(error = NoAuthException())
             }
+            currentUserLiveData?.removeObserver(this)
         }
+    }
+
+    fun requestUser(){
+        currentUserLiveData = Repository.getCurrentUser()
+        currentUserLiveData?.observeForever(currentUserObserver)
+    }
+
+    // Этот метод будет вызван системой при окончательном уничтожении Activity:
+    override fun onCleared() {
+        // убираем подписку в случае, если посреди запроса пользователь решил выйти назад, например
+        currentUserLiveData?.removeObserver(currentUserObserver)
     }
 }
