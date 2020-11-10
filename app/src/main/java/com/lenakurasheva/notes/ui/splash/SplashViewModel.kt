bf1a3efc7a6d55d35f3feaf0b1463111a9f0ccf6
LenaKurasheva <lena.kurasheva@gmail.com>
2020-11-10 21:52:57 +0300

    fix tests for SplashViewModel

--- app/src/main/java/com/lenakurasheva/notes/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/lenakurasheva/notes/ui/splash/SplashViewModel.kt
@@ -1,10 +1,6 @@
 package com.lenakurasheva.notes.ui.splash
 
-import androidx.annotation.VisibleForTesting
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.Observer
 import com.lenakurasheva.notes.data.Repository
-import com.lenakurasheva.notes.data.entity.User
 import com.lenakurasheva.notes.data.errors.NoAuthException
 import com.lenakurasheva.notes.ui.base.BaseViewModel
 import kotlinx.coroutines.launch
--- app/src/test/java/com/lenakurasheva/notes/ui/splash/SplashViewModelTest.kt
+++ app/src/test/java/com/lenakurasheva/notes/ui/splash/SplashViewModelTest.kt
@@ -1,16 +1,11 @@
 package com.lenakurasheva.notes.ui.splash
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.MutableLiveData
 import com.lenakurasheva.notes.data.Repository
 import com.lenakurasheva.notes.data.entity.User
-import com.lenakurasheva.notes.data.errors.NoAuthException
-import io.mockk.clearAllMocks
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
-import junit.framework.Assert.assertEquals
-import org.junit.Assert
+import io.mockk.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -21,48 +16,37 @@ class SplashViewModelTest{
     val taskExecutorRule = InstantTaskExecutorRule()
 
     private val mockRepository = mockk<Repository>()
-    private val currentUserLiveData  = MutableLiveData<User?>()
     private val testUser = User("Ivan", "ivan@ivan")
     private lateinit var viewModel: SplashViewModel
 
     @Before
     fun setup(){
         clearAllMocks()
-//        every {mockRepository.getCurrentUser()} returns currentUserLiveData
+        coEvery { mockRepository.getCurrentUser() } returns testUser
         viewModel = SplashViewModel(mockRepository)
     }
 
     @Test
     fun `should invoke getCurrentUser once`() {
-//        viewModel.requestUser()
-//        verify(exactly = 1) { mockRepository.getCurrentUser() }
+        viewModel.requestUser()
+        coVerify(exactly = 1) { mockRepository.getCurrentUser() }
     }
 
     @Test
-    fun `should return SplashViewState auth true`() {
-//        var result: Boolean? = null
-//        viewModel.viewStateLiveData.observeForever{
-//            result = it.data
-//        }
-//        viewModel.requestUser()
-//        currentUserLiveData.value = testUser
-//        Assert.assertEquals(result, true)
+    fun `should call setData if current user is found`() {
+      GlobalScope.launch {
+          viewModel.requestUser()
+          coVerify(exactly = 1) { viewModel.setData(any()) }
+      }
     }
 
     @Test
-    fun `should return auth error`() {
-//        var result: Throwable? = null
-//        viewModel.viewStateLiveData.observeForever{
-//            result = it.error
-//        }
-//        viewModel.requestUser()
-//        currentUserLiveData.value = null
-//        assertEquals(result?.javaClass,  NoAuthException::class.java)
+    fun `should call setError if current user is not found`() {
+        coEvery { mockRepository.getCurrentUser() } returns null
+        GlobalScope.launch {
+            viewModel.requestUser()
+            coVerify(exactly = 1) { viewModel.setError(any()) }
+        }
     }
 
-    @Test
-    fun `should remove observer`() {
-//        viewModel.onCleared()
-//        Assert.assertFalse(currentUserLiveData.hasObservers())
-    }
 }
